7e7e228f069fbb6d5d1f361e484bbbe77c85a27a
simsgood2 <simsgood2@gmail.com>
2022-11-23 22:02:57 +0900

    병합전

---
+++ compare_codes/compare.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let changeColor = document.querySelector("#build");
+let spanChange = document.querySelectorAll(".yellow");
+
+changeColor.addEventListener("mouseover", (e) => {
+  for (let i = 0; i < spanChange.length; i++) {
+    spanChange[i].classList.add("add");
+  }
+});
